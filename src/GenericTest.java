cf8fc9fe5d3c633391a2dc7edf2c265a7c1b2ddd
VadimPoltoratskiy <vadjan87heavymetall@gmail.com>
2018-10-15 15:27:37 +0300

    First commit

---
+++ src/GenericTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericTest {
+
+    public static void useAnimals(List<? extends Animal> animals) {
+        animals.forEach(Animal::echo);
+    }
+}
+class Species {}
+class Animal extends Species {
+    public void echo() {
+        System.out.println("Raw!");
+    }
+}
+class Cat extends Animal {
+    @Override
+    public void echo() {
+        System.out.println("Meow!");
+    }
+}
---
+++ src/NumberContainer.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NumberContainer {
+
+    private List<Integer> listOfIntegers = null;
+    
+    public void ShowFoo() {
+      for(Integer data : listOfIntegers) {
+          System.out.println(data);
+      }
+    }
+
+    public List<Integer> getListOfIntegers() {
+        return listOfIntegers;
+    }
+
+    public void setListOfIntegers(List<Integer> listOfIntegers) {
+        this.listOfIntegers = listOfIntegers;
+    }
+}
